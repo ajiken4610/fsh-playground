e4362b97561f6cd9025c5e8bbc506fdbc92961da
ajiken4610 <ajiken4610@gmail.com>
2023-10-15 16:43:46 +0900

    リンクの位置を調整

--- components/main/Footer.vue
+++ components/main/Footer.vue
@@ -2,7 +2,7 @@
 div
   .flex.h-8.flex-row.flex-nowrap.items-center
     .ml-auto.flex-none.px-2
-      PartsMarkdown(
+      PartsMarkdown.inline-block(
         md="**Fragment Shader Playground** @ 2023 ajiken4610 licensed under MIT LICENSE. For license information, please see"
       )
       NuxtLink(to="/license") /license
