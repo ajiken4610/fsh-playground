605fc4352c58e6921423d8e95494a97e919c1b7c
ajiken4610 <ajiken4610@gmail.com>
2023-10-15 16:39:39 +0900

    licenseのリンクが機能するように変更

--- components/main/Footer.vue
+++ components/main/Footer.vue
@@ -3,7 +3,8 @@ div
   .flex.h-8.flex-row.flex-nowrap.items-center
     .ml-auto.flex-none.px-2
       PartsMarkdown(
-        md="**Fragment Shader Playground** @ 2023 ajiken4610 licensed under MIT LICENSE. For license information, please see [/#/license](/#/license)"
+        md="**Fragment Shader Playground** @ 2023 ajiken4610 licensed under MIT LICENSE. For license information, please see"
       )
+      NuxtLink(to="/license") /license
   hr.py-1
 </template>
